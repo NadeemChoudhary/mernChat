486471e3516c20b4a8b3966cbe5a6a73971847d9
NadeemChoudhary <nadeemchoudhary37@gmail.com>
2023-05-30 00:04:52 +0530

    first commit

---
+++ src/Modules/form/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Form = () => {
+  return (
+    <>
+      <div className="bg-white  h-[600px] w-[600px] rounded shadow-lg">
+        <h1 className="w-text-xl md:text-5xl lg:text-7xl">hello world</h1>
+      </div>
+    </>
+  );
+};
